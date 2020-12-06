711ef9d5a4276c9a52a75fecd0086fd7a0a049ad
pedromoreiramota <68519461+pedromoreiramota@users.noreply.github.com>
2020-12-06 00:42:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+  document.getElementById('menu').style.width = '400px';
+}
+function fecharMenu(){
+  document.getElementById('menu').style.width = '0px';
+}
+
+
+//Q.HTML CONFIRMAÇÃO PARA REDIRECIONAMENTO
